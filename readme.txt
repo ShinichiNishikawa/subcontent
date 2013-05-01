5a5d1dbe309acc868c0b9d8221b8d2506929135f
Shinichi Nishikawa <shinichi.nishikawa@gmail.com>
2013-05-01 22:45:07 +0900

    first commit

---
+++ readme.txt
@@ -0,0 +1,82 @@
+=== Subtitle ===
+Contributors: ShinichiN
+Tags: sub content, editor, wp_editor, custom field
+Requires at least: 3.5.1
+Tested up to: 3.5.1
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds another text editor below the editor in your post/page/custom-post-type edit page.
+
+== Description ==
+
+Adds another text editor below the editor in your post/page/custom-post-type edit page.
+
+* [GitHub](https://github.com/ShinichiNishikawa/subcontent)
+* [Plugin Homepage](http://nskw-style.com/2013/wordpress/plugin-downloads/subcontent.html)
+
+= Retriving and displaying in templates =
+
+Use `get_the_subcontent()` to retrieve and `the_subcontent()` to display.
+
+= Change label =
+
+By default the label of the input field is "Subtitle". There's a hook for changing it.
+
+`add_filter( 'nskw-sce-meta_label', 'nskw_changesubcontentlabel' );
+function nskw_changesubcontentlabel() {
+    return 'Contact Information';
+}`
+
+= Change the height of the editor =
+
+Default it's 10.
+
+`add_filter( 'nskw-sce-row', 'change_subcontent_row' );
+function change_subcontent_row() {
+    return 20;
+}`
+
+= Hide in particular post type =
+
+`add_filter( 'nskw-sce-post_type', 'nskw_hide_subcontent' );
+function nskw_hide_subcontent() {
+    return array( 'page', 'newposttype' );
+}`
+
+= Disable wpautop =
+
+`add_filter( 'nskw-sce-wpautop', 'nskw_change_wpautop' );
+function nskw_change_wpautop() {
+    return false;
+}`
+
+= No Media Uploader =
+
+`add_filter( 'nskw-sce-media_buttons', 'nskw_change_mediasetting' );
+function nskw_change_mediasetting() {
+    return false;
+}`
+
+= No tinymce =
+
+`add_filter( 'nskw-sce-tinymce', 'nskw_cange_tinymce' );
+function nskw_cange_tinymce() {
+    return false;
+}`
+
+== Installation ==
+
+1. Upload `subcontent` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Screenshots ==
+
+1. Another editor for subcontent appears!
+
+== Changelog ==
+
+0.1 First Release.
+
---
+++ subcontent.php
@@ -0,0 +1,96 @@
+<?php
+/*
+Plugin Name: Subcontent
+Plugin URI: http://nskw-style.com/2013/wordpress/plugin-downloads/subcontent.html ‎
+Description: This plugin adds another editor to your post/page/customposttype edit page.
+Author: Shinichi Nishikawa
+Version: 0.1
+Author URI: http://en.nskw-style.com
+*/
+
+class SubContentEditor {
+
+public $meta_key = 'subcontent';
+public $meta_label = 'Subcontent';
+public $post_type = array( 'page' );
+public $wpautop = true;
+
+// add actions
+function __construct()
+{
+	add_action( 'edit_form_after_editor', array( &$this, 'edit_form_after_editor' ) );
+	add_action( 'save_post', array( &$this, 'save_post' ) );
+	add_action( 'init', array( &$this, 'init' ) );
+}
+
+public function init() {
+	$this->meta_key = apply_filters( 'nskw-sce-meta_key', $this->meta_key );
+	$this->post_type = apply_filters( 'nskw-sce-post_type', $this->post_type );
+	$this->meta_label = apply_filters( 'nskw-sce-meta_label', $this->meta_label );
+	$this->wpautop = apply_filters( 'nskw-sce-wpautop', $this->wpautop );
+}
+
+// display the editor
+public function edit_form_after_editor()
+{
+	// return if not admin panel
+	if ( !is_admin() ) {
+		return;
+	}
+	// return if post type is in the deny array
+	if ( in_array( get_post_type( $GLOBALS['post'] ), $this->post_type ) ) {
+		return;
+	}
+
+	$value = $this->get_the_subcontent();
+	
+	$sc_arg = array(
+		'textarea_rows' => apply_filters( 'nskw-sce-row', 10 ),
+		'wpautop' => $this->wpautop,
+		'media_buttons' => apply_filters( 'nskw-sce-media_buttons', true ),
+		'tinymce' => apply_filters( 'nskw-sce-tinymce', true ),
+	);
+	
+	echo '<h3 class="subcontentLabel" style="margin-top:15px;">' . $this->meta_label . '</h3>';
+	wp_editor( $value, 'subcontent', $sc_arg );
+	
+}
+
+// save the value
+public function save_post( $post_id )
+{
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
+    
+    if ( isset ( $_POST[ $this->meta_key ] ) ) {
+    	return update_post_meta( $post_id, $this->meta_key, $_POST[ $this->meta_key ] );
+    }
+    delete_post_meta( $post_id, $this->meta_key );
+
+}
+
+public function get_the_subcontent() {
+	global $post;
+	$subcontent = get_post_meta( $post->ID, $this->meta_key, true );
+	if ( $this->wpautop == true ) {
+		return wpautop( $subcontent );
+	} else {
+		return $subcontent;
+	}
+}
+
+}// class ここまで
+$nskwSCE = new SubContentEditor();
+
+function get_the_subcontent() {
+	global $nskwSCE;
+	return $nskwSCE->get_the_subcontent();
+}
+
+function the_subcontent() {
+	echo get_the_subcontent();
+}
