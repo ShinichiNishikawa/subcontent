c45a1f2c3e060004b00bbc331f1430b42884fcb8
Shinichi Nishikawa <shinichi.nishikawa@gmail.com>
2014-06-30 18:40:12 +0700

    0.2 tested

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: ShinichiN
 Tags: sub content, editor, wp_editor, custom field
 Requires at least: 3.5.1
-Tested up to: 3.5.1
-Stable tag: 0.1
+Tested up to: 3.9.1
+Stable tag: 0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -66,6 +66,9 @@ function nskw_cange_tinymce() {
     return false;
 }`
 
+= No shortcode rendering =
+`add_filter( 'nskw-sce-shortcode', '__return_false' );`
+
 == Installation ==
 
 1. Upload `subcontent` folder to the `/wp-content/plugins/` directory
@@ -79,4 +82,7 @@ function nskw_cange_tinymce() {
 == Changelog ==
 
 0.1 First Release.
+
+0.2 Merged [dcondrey's Pull Request](https://github.com/ShinichiNishikawa/subcontent/commit/2c53a44d1380fa8e271d353af29cf6c245df4406), but changed the order of wpautop and do_shortcode.
+
 
--- subcontent.php
+++ subcontent.php
@@ -4,7 +4,7 @@ Plugin Name: Subcontent
 Plugin URI: http://nskw-style.com/2013/wordpress/plugin-downloads/subcontent.html ‎
 Description: This plugin adds another editor to your post/page/customposttype edit page.
 Author: Shinichi Nishikawa
-Version: 0.1
+Version: 0.2
 Author URI: http://en.nskw-style.com
 */
 
@@ -14,20 +14,22 @@ public $meta_key   = 'subcontent';
 public $meta_label = 'Subcontent';
 public $post_type  = array( 'page' );
 public $wpautop    = true;
+public $shortcode  = true;
 
 // add actions
 function __construct()
 {
 	add_action( 'edit_form_after_editor', array( &$this, 'edit_form_after_editor' ) );
-	add_action( 'save_post', array( &$this, 'save_post' ) );
-	add_action( 'init', array( &$this, 'init' ) );
+	add_action( 'save_post',              array( &$this, 'save_post' ) );
+	add_action( 'init',                   array( &$this, 'init' ) );
 }
 
 public function init() {
-	$this->meta_key   = apply_filters( 'nskw-sce-meta_key', $this->meta_key );
-	$this->post_type  = apply_filters( 'nskw-sce-post_type', $this->post_type );
+	$this->meta_key   = apply_filters( 'nskw-sce-meta_key',   $this->meta_key );
+	$this->post_type  = apply_filters( 'nskw-sce-post_type',  $this->post_type );
 	$this->meta_label = apply_filters( 'nskw-sce-meta_label', $this->meta_label );
-	$this->wpautop    = apply_filters( 'nskw-sce-wpautop', $this->wpautop );
+	$this->wpautop    = apply_filters( 'nskw-sce-wpautop',    $this->wpautop );
+	$this->shortcode  = apply_filters( 'nskw-sce-shortcode',  $this->shortcode );
 }
 
 // display the editor
@@ -75,12 +77,18 @@ public function save_post( $post_id )
 
 public function get_the_subcontent() {
 	global $post;
-	$subcontent = do_shortcode( get_post_meta( $post->ID, $this->meta_key, true ) );
+	$subcontent = get_post_meta( $post->ID, $this->meta_key, true );
+
 	if ( $this->wpautop == true ) {
-		return wpautop( $subcontent );
-	} else {
-		return $subcontent;
+		$subcontent = wpautop( $subcontent );
 	}
+
+	if ( true == $this->shortcode ) {
+		$subcontent = do_shortcode( $subcontent );
+	}
+
+	return $subcontent;
+	
 }
 
 }// class
